2876d3af010735f4b25f273ab6a1f9b1bba57a01
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-04 15:01:47 +0200

    refactor: update import path

--- libs/heroes/feature-page/src/lib/heroes-feature-page.module.ts
+++ libs/heroes/feature-page/src/lib/heroes-feature-page.module.ts
@@ -1,4 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-@NgModule({})
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, HeroesRoutingModule],
+  declarations: [HeroListComponent, HeroDetailComponent],
+})
 export class HeroesFeaturePageModule {}

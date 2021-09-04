39d4933a8be61d060647d817c840ca8ddc9ac623
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-04 15:31:43 +0200

    refactor: update lazy import

--- libs/crisis-center/feature-page/src/lib/crisis-center-feature-page.module.ts
+++ libs/crisis-center/feature-page/src/lib/crisis-center-feature-page.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { CrisisCenterHomeComponent } from './crisis-center-home/crisis-center-home.component';
+import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, CrisisCenterRoutingModule],
+  declarations: [
+    CrisisCenterComponent,
+    CrisisListComponent,
+    CrisisCenterHomeComponent,
+    CrisisDetailComponent,
+  ],
 })
 export class CrisisCenterFeaturePageModule {}

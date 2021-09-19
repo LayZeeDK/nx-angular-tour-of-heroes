3741e9f4caafce158a5eae45266fc24763c3f6fa
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-19 23:04:57 +0200

    feat: generate crisis center data access library

---
+++ libs/crisis-center/data-access/src/test.ts
@@ -0,0 +1,44 @@
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import 'zone.js';
+import 'zone.js/testing';
+
+import { ComponentFixtureAutoDetect, getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
+    keys(): string[];
+    <T>(id: string): T;
+  };
+};
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+  {
+    teardown: {
+      destroyAfterEach: true,
+      rethrowErrors: false,
+    },
+  }
+);
+getTestBed().configureCompiler({
+  providers: [
+    {
+      provide: ComponentFixtureAutoDetect,
+      useValue: true,
+    },
+  ],
+});
+// Then we find all the tests.
+const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);

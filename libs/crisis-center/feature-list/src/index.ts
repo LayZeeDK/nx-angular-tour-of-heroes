35e0258f7661c20837d725fb5a3740d41044b300
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-19 23:26:34 +0200

    refactor: add list route

--- libs/crisis-center/feature-list/src/index.ts
+++ libs/crisis-center/feature-list/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/crisis-center-feature-list.module';
+export * from './lib/crisis-list/crisis-list.component';
--- libs/crisis-center/feature-shell/src/lib/crisis-center-feature-shell.module.ts
+++ libs/crisis-center/feature-shell/src/lib/crisis-center-feature-shell.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrisisCenterFeatureListModule, CrisisListComponent } from '@tour-of-heroes/crisis-center/feature-list';
 
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { CrisisCenterModule } from './crisis-center/crisis-center.module';
@@ -8,12 +9,22 @@ const crisisCenterRoutes: Routes = [
   {
     path: '',
     component: CrisisCenterComponent,
-    children: [],
+    children: [
+      {
+        path: '',
+        component: CrisisListComponent,
+        children: [],
+      },
+    ],
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(crisisCenterRoutes), CrisisCenterModule],
+  imports: [
+    RouterModule.forChild(crisisCenterRoutes),
+    CrisisCenterModule,
+    CrisisCenterFeatureListModule,
+  ],
   exports: [RouterModule],
 })
 export class CrisisCenterFeatureShellModule {}

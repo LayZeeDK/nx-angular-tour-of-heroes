162fb0df4c8593f01d4fdd023dae54834a42621d
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-19 22:32:33 +0200

    refactor: move shell component to shell library

--- libs/crisis-center/feature-shell/src/lib/crisis-center-feature-shell.module.ts
+++ libs/crisis-center/feature-shell/src/lib/crisis-center-feature-shell.module.ts
@@ -1,4 +1,19 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-@NgModule({})
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { CrisisCenterModule } from './crisis-center/crisis-center.module';
+
+const crisisCenterRoutes: Routes = [
+  {
+    path: '',
+    component: CrisisCenterComponent,
+    children: [],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(crisisCenterRoutes), CrisisCenterModule],
+  exports: [RouterModule],
+})
 export class CrisisCenterFeatureShellModule {}
---
+++ libs/crisis-center/feature-shell/src/lib/crisis-center/crisis-center.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { CrisisCenterComponent } from './crisis-center.component';
+
+@NgModule({
+  declarations: [CrisisCenterComponent],
+  imports: [RouterModule],
+})
+export class CrisisCenterModule {}

676470eb3870e904ddc1414ba654e9a3399430af
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-20 22:41:08 +0200

    refactor: move crisis detail component to crisis detail feature

--- libs/crisis-center/feature-detail/src/lib/crisis-center-feature-detail.module.ts
+++ libs/crisis-center/feature-detail/src/lib/crisis-center-feature-detail.module.ts
@@ -1,4 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-@NgModule({})
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
+import { CrisisDetailModule } from './crisis-detail/crisis-detail.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CrisisDetailComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes), CrisisDetailModule],
+})
 export class CrisisCenterFeatureDetailModule {}

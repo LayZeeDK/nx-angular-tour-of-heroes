0ba3e38672dbda246ed71b777ced46763fe1b338
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-20 22:37:38 +0200

    refactor: move crisis center home component to home feature

--- libs/crisis-center/feature-home/src/lib/crisis-center-feature-home.module.ts
+++ libs/crisis-center/feature-home/src/lib/crisis-center-feature-home.module.ts
@@ -1,4 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-@NgModule({})
+import { CrisisCenterHomeComponent } from './crisis-center-home/crisis-center-home.component';
+import { CrisisCenterHomeModule } from './crisis-center-home/crisis-center-home.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CrisisCenterHomeComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes), CrisisCenterHomeModule],
+})
 export class CrisisCenterFeatureHomeModule {}

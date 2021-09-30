5d6ab5f62776055cb5137df04ab77b209a56595c
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-30 15:42:09 +0200

    refactor: move admin dashboard component to admin dashboard feature

--- libs/admin/feature-dashboard/src/lib/admin-feature-dashboard.module.ts
+++ libs/admin/feature-dashboard/src/lib/admin-feature-dashboard.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminDashboardModule } from './admin-dashboard/admin-dashboard.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminDashboardComponent,
+  },
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [RouterModule.forChild(routes), AdminDashboardModule],
 })
 export class AdminFeatureDashboardModule {}

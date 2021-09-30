ad184c14d2b3a74249344d1c140ce5ed3723cc76
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-30 15:42:26 +0200

    refactor: update import path

--- libs/admin/feature-shell/src/lib/admin-feature-shell.module.ts
+++ libs/admin/feature-shell/src/lib/admin-feature-shell.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AdminComponent } from './admin.component';
+import { AdminComponent } from './admin/admin.component';
 import { AdminModule } from './admin/admin.module';
 
 const routes: Routes = [

3a32daaa2420223d9796ed923291caea2693ebd2
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-30 16:17:53 +0200

    refactor: move login component to login feature

--- libs/auth/feature-login/src/lib/auth-feature-login.module.ts
+++ libs/auth/feature-login/src/lib/auth-feature-login.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { LoginModule } from './login/login.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [RouterModule.forChild(routes), LoginModule],
 })
 export class AuthFeatureLoginModule {}
---
+++ libs/auth/feature-login/src/lib/login/login.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [CommonModule],
+})
+export class LoginModule {}

b27eb8e4cb45b570374373b21774b7e2cbf0dd48
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-10-24 22:51:30 +0200

    test: explain Angular Testing Library setting

--- libs/shared/test-util-staging/src/lib/set-up-angular-testing-library.ts
+++ libs/shared/test-util-staging/src/lib/set-up-angular-testing-library.ts
@@ -2,6 +2,7 @@ import { configure } from '@testing-library/angular';
 
 export function setUpAngularTestingLibrary(): void {
   configure({
+    // Assume SCAMs
     excludeComponentDeclaration: true,
     dom: {
       throwSuggestions: true,
